34ba157ed6225c223dc658d10ba0ec1b30ea0caf
Giovanni Bertolucci <gbertolucci@gmail.com>
2015-02-15 20:09:18 +0000

    Configured my first rest json web service like in

--- src/main/java/com/giobertox/controller/ActivityController.java
+++ src/main/java/com/giobertox/controller/ActivityController.java
@@ -5,17 +5,19 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.giobertox.model.Activity;
 
 @RestController
+@RequestMapping("/rest/activities/")
 public class ActivityController {
 
-	@RequestMapping(value = "/activities", method = RequestMethod.GET)
-	// , produces = "application/json")
-	public @ResponseBody List<Activity> findAllActivities() {
+	@RequestMapping(method = RequestMethod.GET, headers = "Accept=application/json")
+	// (value = "/rest/activities/")
+	// public List<Activity> findAllActivities(@RequestParam(value="name",
+	// defaultValue="World") String name)
+	public List<Activity> findAllActivities() {
 		List<Activity> activities = new ArrayList<Activity>();
 
 		Activity run = new Activity(1, "run");

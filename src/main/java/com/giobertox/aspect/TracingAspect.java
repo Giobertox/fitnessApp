57ab56c1e7c9fc0120980937a33c6ed03f5c41b6
Bertolucci, Giovanni <Giovanni.Bertolucci@specialistholidays.com>
2015-02-19 09:33:11 +0000

    Started work on Aspect AOP ,  added libraries to pom and created basic class

---
+++ src/main/java/com/giobertox/aspect/TracingAspect.java
@@ -0,0 +1,44 @@
+package com.giobertox.aspect;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TracingAspect {
+
+	Logger logger = Logger.getLogger(this.getClass());
+
+	boolean enteringCalled = false;
+
+	@Pointcut("execution(public void methodName(javax.portlet.PortletRequest.PortletRequest)")
+	private void verySpecificMethods() {
+	}
+
+	@Pointcut("execution(* * *(..)")
+	private void anyOperation() {
+	}
+
+	@Pointcut("execution(public * *(..)")
+	private void anyPublicOperation() {
+	}
+
+	@Before("execution(* *(..)")
+	public void logEntering(JoinPoint joinPoint) {
+		enteringCalled = true;
+		logger.trace("entering " + joinPoint.getStaticPart().getSignature().toString());
+	}
+
+	public boolean isEnteringCalled() {
+		return enteringCalled;
+	}
+
+	public void setEnteringCalled(boolean enteringCalled) {
+		this.enteringCalled = enteringCalled;
+	}
+
+}

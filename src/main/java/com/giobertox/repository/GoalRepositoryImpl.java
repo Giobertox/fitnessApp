b2ba64bec853cc1cd969f5b54b508591e6e98f4e
Giovanni Bertolucci <gbertolucci@gmail.com>
2015-03-24 17:51:52 +0000

    Using NamedQueries and Projection to define better what data to provide to UI and more efficiently

--- src/main/java/com/giobertox/repository/GoalRepositoryImpl.java
+++ src/main/java/com/giobertox/repository/GoalRepositoryImpl.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
@@ -13,8 +13,6 @@ import com.giobertox.model.GoalReport;
 
 @Repository("goalRepository")
 public class GoalRepositoryImpl implements GoalRepository {
-
-	public static final String FIND_ALL_GOALS = "SELECT g FROM Goal g";
 
 	@PersistenceContext
 	private EntityManager em;
@@ -26,21 +24,20 @@ public class GoalRepositoryImpl implements GoalRepository {
 		return goal;
 	}
 
-	@SuppressWarnings({ "unchecked" })
 	@Override
 	public List<Goal> loadAll() {
 		// Query query = em.createQuery("Select g from Goal g");
-		Query query = em.createQuery(FIND_ALL_GOALS, Goal.class);
+		TypedQuery<Goal> query = em.createNamedQuery(Goal.FIND_ALL_GOALS, Goal.class);
 		return query.getResultList();
 	}
 
 	@Override
 	public List<GoalReport> findAllGoalReports() {
-		Query query = em
-				.createQuery("Select new com.giobertox.model.GoalReport(g.minutes, e.minutes, e.type) "
-						+ "from Goal g, Exercise e where g.id = e.goal.id");
-		// TypedQuery<GoalReport> query = em
-		// .createNamedQuery(Goal.FIND_GOAL_REPORTS, GoalReport.class);
+		// Query query = em
+		// .createQuery("Select new com.giobertox.model.GoalReport(g.minutes, e.minutes, e.type) "
+		// + "from Goal g, Exercise e where g.id = e.goal.id");
+		TypedQuery<GoalReport> query = em
+				.createNamedQuery(Goal.FIND_GOAL_REPORTS, GoalReport.class);
 		return query.getResultList();
 	}
 }

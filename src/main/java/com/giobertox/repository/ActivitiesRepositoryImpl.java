55b4b91a4291a1f65bcb24bfec30ef13acb1f0ec
Giovanni Bertolucci <gbertolucci@gmail.com>
2015-04-18 15:11:06 +0100

    fixed  , annotation of the activities  repository

--- src/main/java/com/giobertox/repository/ActivitiesRepositoryImpl.java
+++ src/main/java/com/giobertox/repository/ActivitiesRepositoryImpl.java
@@ -3,8 +3,11 @@ package com.giobertox.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.giobertox.model.Activity;
 
+@Repository
 public class ActivitiesRepositoryImpl implements ActivitiesRepository {
 
 	private List<Activity> activities = new ArrayList<Activity>();

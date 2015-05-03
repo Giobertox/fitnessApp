23249b1756b916d92c9fd9fae54c1971185aa6f2
Giovanni Bertolucci <gbertolucci@gmail.com>
2015-05-03 19:11:29 +0100

    added test for Builder full params constructor

--- src/main/test/java/com/giobertox/pattern/creational/ClassWithBuilderTest.java
+++ src/main/test/java/com/giobertox/pattern/creational/ClassWithBuilderTest.java
@@ -7,11 +7,17 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.giobertox.pattern.creational.ClassWithBuilder.Builder;
+
 /**
  * @author giovanni
  *
  */
 public class ClassWithBuilderTest {
+
+	private static final String SAUCE = "sauce";
+	private static final String MEAT = "meat";
+	private static final String BREAD = "bread";
 
 	/**
 	 * @throws java.lang.Exception
@@ -23,12 +29,27 @@ public class ClassWithBuilderTest {
 	@Test
 	public void test_basicConstructor_success() {
 		// Setup
-		ClassWithBuilder target = new ClassWithBuilder.Builder("bread").build();
+		ClassWithBuilder target = new ClassWithBuilder.Builder(BREAD).build();
 
 		// Execute
 
 		// Verification
-		Assert.assertEquals("bread", target.getBread());
+		Assert.assertEquals(BREAD, target.getBread());
+	}
+
+	@Test
+	public void test_AllFieldConstructor_success() {
+		// Setup
+		ClassWithBuilder target;
+		Builder builder = new ClassWithBuilder.Builder(BREAD).meat(MEAT).sauce(SAUCE);
+		target = builder.build();
+
+		// Execute
+
+		// Verification
+		Assert.assertEquals(BREAD, target.getBread());
+		Assert.assertEquals(MEAT, target.getMeat());
+		Assert.assertEquals(SAUCE, target.getSauce());
 	}
 
 }
